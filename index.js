0e6698e23b6d438bea2c8f84b70d50699cf57063
Blake Romero <65886388+blakerom@users.noreply.github.com>
2020-06-24 11:04:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 'use strict';
 Parameters.collection = [];
 var days = [];
-
 var parametersForTable = ['Nitrates:', 'Alkalinity:', 'Calcium:','Magnesium:', 'Salinity:', 'Temperature:'];
+
 
 var stringyParameters = localStorage.getItem('params');
 var parameters = JSON.parse(stringyParameters);
 
 if (parameters) {
   Parameters.collection = parameters;
-
+  
   var nitDataset = [];
   var alkDataset = [];
   var calDataset = [];
@@ -48,7 +48,7 @@ function handleSubmit(event) {
   var theFormForM = parseFloat(event.target.magnesium.value);
   var theFormForS = parseFloat(event.target.salinity.value);
   var theFormForT = parseFloat(event.target.temperature.value);
-
+  
   for (var i = 0; i < Parameters.collection.length; i++) {
     nitDataset.push(Parameters.collection[i].nitrate);
     alkDataset.push(Parameters.collection[i].alkalinity);
@@ -59,17 +59,26 @@ function handleSubmit(event) {
   }
 
   var newDayData = new Parameters(theFormForN, theFormForA, theFormForC, theFormForM, theFormForS, theFormForT);
+  console.log(newDayData);
+
   var stringyParamData = JSON.stringify(Parameters.collection);
   localStorage.setItem('params', stringyParamData);
   createGraph();
 }
 
-
-
-
-
-
 function createGraph() {
+  var nitDataset = [];
+  var alkDataset = [];
+  var calDataset = [];
+  console.log(Parameters.collection);
+  for (var i = 0; i < Parameters.collection.length; i++) {
+    nitDataset.push(Parameters.collection[i].nitrate);
+    alkDataset.push(Parameters.collection[i].alkalinity);
+    calDataset.push(Parameters.collection[i].calcium);
+  }
+  // console.log('Check nitrate array ', nitDataset);
+  // console.log('Check alkaline array ', alkDataset);
+  // console.log('Check calcium array ', calDataset);
 
   var ctx = document.getElementById('myChart').getContext('2d');
   var productChart = new Chart(ctx, {
@@ -80,36 +89,48 @@ function createGraph() {
       datasets: [{
         label: parametersForTable[0],
         data: nitDataset,
-        // backgroundColor: 'blue',
+        backgroundColor: 'blue',
         borderColor: 'blue'
-      },
-      {
-        label: parametersForTable[1],
-        data: alkDataset,
-        // backgroundColor: 'red',
-        borderColor: 'red',
-      },
-      {
-        label: parametersForTable[2],
-        data: calDataset,
-        // backgroundColor: 'green',
-        borderColor: 'green',
       }]
     },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-    }
+    options: {}
   });
+
+  var dtx = document.getElementById('myChart2').getContext('2d');
+  var productChart = new Chart(dtx, {
+    type: 'line',
+
+    data: {
+      labels: days,
+      datasets: [{
+        label: parametersForTable[1],
+        data: alkDataset,
+        backgroundColor: 'red',
+        borderColor: 'red'
+      }]
+    },
+    options: {}
+  });
+
+  var etx = document.getElementById('myChart3').getContext('2d');
+  var productChart = new Chart(etx, {
+    type: 'line',
+
+    data: {
+      labels: days,
+      datasets: [{
+        label: parametersForTable[2],
+        data: calDataset,
+        backgroundColor: 'green',
+        borderColor: 'green'
+      }]
+    },
+    options: {}
+  });
+
 }
 
 function Parameters(nitrate, alkalinity, calcium, magnesium, salinity, temp) {
-
   this.nitrate = nitrate;
   this.alkalinity = alkalinity;
   this.calcium = calcium;
@@ -122,10 +143,13 @@ function Parameters(nitrate, alkalinity, calcium, magnesium, salinity, temp) {
   let today = new Date().toLocaleDateString();
   days.push(today);
 }
+
 var newDay = new Date();
 Parameters.prototype.date = newDay;
 
-//var newParameter = new Parameters(5, 6, 7, 1300, 1.025, 80);
-//var newParameter2 = new Parameters(8, 9, 10, 1250, 1.020, 78);
-// var newParameter3 = new Parameters(11, 12, 13);
-// var newParameter4 = new Parameters(1, 2, 3);
+var newParameter = new Parameters(20, 9, 400); //1300 mag, 1.025 sal, 78 temp
+var newParameter2 = new Parameters(12, 8, 380); 
+var newParameter3 = new Parameters(11, 7, 350);
+var newParameter4 = new Parameters(14, 10, 375);
+
+createGraph();
